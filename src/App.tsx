3686a8d2e64c15b4205ffe4846224c4c1a4bf273
Materka11 <arekmaterka11@gmail.com>
2022-08-27 17:32:43 +0200

    Modified spotlight and added max distance to OrbitControls

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,18 @@ import Basement from './Basement';
 import { SpotLight, SpotLightHelper } from 'three';
 
 function App() {
-
-	const spotlightRef = useRef<SpotLight>(null!);
-	useHelper(spotlightRef, SpotLightHelper, 'red');
-
-	const { distance, intensity, angle } = useControls({ distance: { value: 5, min: 1, max: 10 }, intensity: {value: 1, min: 0, max: 5}, angle: {value: Math.PI / 6, min: 0.1, max: 1} });
 	return (
 		<>
-			<spotLight color={'#3588FF'} ref={spotlightRef} position={[ 0, 5, 0 ]} distance={distance} intensity={intensity} angle={angle} />
+			<spotLight color={'#3588FF'} position={[ 0, 5, 0 ]} distance={10} intensity={10} angle={0.25} penumbra={1} />
 			<Suspense fallback={null}>
 				<Basement position={[ 0, -1, 0 ]} />
 				<mesh position={[ 0, -1, 0 ]} rotation={[ -angleToRadians(90), 0, 0 ]} receiveShadow >
 					<planeGeometry args={[ 100, 100 ]} />
 					<meshStandardMaterial color={'#000000'} />
 				</mesh>
+				
 			</Suspense>
-			<OrbitControls maxPolarAngle={Math.PI / 2} />
+			<OrbitControls maxPolarAngle={Math.PI / 2} maxDistance={100}/>
 			<color attach="background" args={[ '#000000' ]} />
             </>
 	);
